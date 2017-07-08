679578ab43b24833c5bde05a9a40b48618779b49
Azim <azimsadikov@gmail.com>
2017-07-08 12:52:45 -0500

    hashsets is done

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> Showroom = new HashSet<string>();
+            Showroom.Add("Honda");
+            Showroom.Add("Tesla");
+            Showroom.Add("Toyota");
+            Showroom.Add("Mercedez");
+            // Console.Write($"There are {Showroom.Count} cars in the Showroom");
+            Showroom.Add("Mercedez");
+            Console.Write($"There are {Showroom.Count} cars in the Showroom");
+            HashSet<string> UsedLot = new HashSet<string>();
+            UsedLot.Add("Buggatti");
+            UsedLot.Add("Ferrari");
+            Showroom.UnionWith(UsedLot);
+            Console.Write($"There are now {Showroom.Count} cars in the Showroom");
+            Showroom.Remove("Ferrari");
+            Console.Write($"I sold one car and There are now {Showroom.Count} cars in the Showroom");
+            HashSet<string> Junkyard = new HashSet<string>();
+            Junkyard.Add("Buggatti");
+            Junkyard.Add("Ferrari");
+            Junkyard.Add("Honda");
+            Junkyard.Add("Lambo");
+            Junkyard.IntersectWith(Showroom);
+            foreach (string car in Junkyard)
+            {
+                Console.WriteLine(car);
+            }
+            Junkyard.UnionWith(Showroom);
+            Junkyard.Remove("Honda");
+        }
+    }
+}
